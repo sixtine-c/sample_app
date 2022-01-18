7e927d5ab9e439858adbbb6fa29447ad52b54de0
Sixtine Caspar <sixcaspar@gmail.com>
2022-01-18 15:33:03 +0100

    added error messages in the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(users_params)
+    if @user.save
+    else
+      render "new"
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def users_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
