b5388149b5d6225e571115275e3742959189017a
Sixtine Caspar <sixcaspar@gmail.com>
2022-02-04 16:06:25 +0100

    done login + change in the header + reposnsive header footer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user
     # Log the user in and redirect to the user's show page.
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+
+  # This places a temporary cookie on the user’s browser containing an encrypted
+  # version of the user’s id, which
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user = @current_user || User.find_by(id: session[:user_id])
+      # if @current_user.nil?
+      #   # we don't use the find method as it would raise an exception if the user_id is nil, which is often in that case
+      #   @current_user = User.find_by(id: session[:user_id])
+      # else
+      #   @current_user
+      # end
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
